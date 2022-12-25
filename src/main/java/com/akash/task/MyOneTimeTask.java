491c1039526e828d9949aaa5f0eaf5189e5f2e77
Akash Sharma <akash777.sharma@gmail.com>
2022-12-25 18:56:27 +0530

    Update MyOneTimeTask.java

--- src/main/java/com/akash/task/MyOneTimeTask.java
+++ src/main/java/com/akash/task/MyOneTimeTask.java
@@ -3,7 +3,9 @@ package com.akash.task;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public class MyOneTimeTask implements Runnable {
+public class MyOneTimeTask implements Runnable, Serializable {
+
+  private static final long serialVersionUID = 4357644690L;
 
   private static final Logger LOGGER = LogManager.getLogger(MyOneTimeTask.class);
 
